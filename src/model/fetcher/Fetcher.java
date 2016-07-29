de24c6bd831a7e9ccc4f19668d9fd1fa7c0ef35d
adamtache <at201@duke.edu>
2016-07-29 16:48:37 -0400

    Setup fetcher inheritance structure

---
+++ src/model/fetcher/Fetcher.java
@@ -0,0 +1,43 @@
+package model.fetcher;
+
+import java.io.IOException;
+
+import org.jsoup.select.Elements;
+
+public abstract class Fetcher {
+	
+	private long lastRequestTime = -1;
+	private long minInterval = 1000;
+	
+	public Fetcher(){
+	}
+	
+	public Fetcher(long lastRequestTime, long minInterval){
+		this.lastRequestTime = lastRequestTime;
+		this.minInterval = minInterval;
+	}
+	
+	public abstract Elements read(String url) throws IOException;
+	
+	public abstract Elements fetch(String url) throws IOException;
+
+	/**
+	 * Rate limits by waiting at least the minimum interval between requests.
+	 */
+	public void sleepIfNeeded() {
+		if (lastRequestTime != -1) {
+			long currentTime = System.currentTimeMillis();
+			long nextRequestTime = lastRequestTime + minInterval;
+			if (currentTime < nextRequestTime) {
+				try {
+					//System.out.println("Sleeping until " + nextRequestTime);
+					Thread.sleep(nextRequestTime - currentTime);
+				} catch (InterruptedException e) {
+					System.err.println("Warning: sleep interrupted in fetchWikipedia.");
+				}
+			}
+		}
+		minInterval = System.currentTimeMillis();
+	}
+	
+}
---
+++ src/model/fetcher/WikiFetcher.java
@@ -0,0 +1,63 @@
+package model.fetcher;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
+public class WikiFetcher extends Fetcher{
+
+	/**
+	 * Fetches and parses a URL string, returning a list of paragraph elements.
+	 *
+	 * @param url
+	 * @return
+	 * @throws IOException
+	 */
+	public Elements fetch(String url) throws IOException {
+		sleepIfNeeded();
+
+		// download and parse the document
+		Connection conn = Jsoup.connect(url);
+		Document doc = conn.get();
+
+		// select the content text and pull out the paragraphs.
+		Element content = doc.getElementById("mw-content-text");
+
+		// TODO: avoid selecting paragraphs from sidebars and boxouts
+		Elements paras = content.select("p");
+		return paras;
+	}
+
+	/**
+	 * Reads the contents of a Wikipedia page from src/resources.
+	 *
+	 * @param url
+	 * @return
+	 * @throws IOException
+	 */
+	public Elements read(String url) throws IOException {
+		URL realURL = new URL(url);
+
+		// assemble the file name
+		String slash = File.separator;
+		String filename = "resources" + slash + realURL.getHost() + realURL.getPath();
+
+		// read the file
+		InputStream stream = WikiFetcher.class.getClassLoader().getResourceAsStream(filename);
+		Document doc = Jsoup.parse(stream, "UTF-8", filename);
+
+		// TODO: factor out the following repeated code
+		Element content = doc.getElementById("mw-content-text");
+		Elements paras = content.select("p");
+		return paras;
+	}
+	
+}
